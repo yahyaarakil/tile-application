7a01bf45c6aa9b269164c9ef3d8d4067fdd9cc9c
Yahya Arakil <yahyaarakil@gmail.com>
2022-05-18 19:02:36 +0300

    Added and tested find user by email

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,27 +1,73 @@
 const dbConnection = require('../db_connection');
 
 class User {
-    constructor ({ email, fullName, teleNumber, sessionID }) {
-        this.email = email;
-        this.fullName = fullName;
-        this.teleNumber = teleNumber;
-        this.sessionID = sessionID;
+    constructor ({ Email, Password, FullName, Role, telephoneNumbers, inDB }) {
+        this.email = Email;
+        this.password = Password;
+        this.fullName = FullName;
+        this.role = Role;
+        this.inDB = inDB ? inDB : false;
+
+        this.telephoneNumbers = [...telephoneNumbers];
     }
 
-    findByEmail(email) {
+    static async findByEmail(email) {
         return new Promise((resolve, reject) => {
-            dbConnection.makeQuery('SELECT * FROM TILE_USER WHERE email = ?', [ email ]).then(([ results ]) => {
+            dbConnection.makeQuery('SELECT * FROM TileUsers WHERE Email=?', [ email ]).then(([ results ]) => {
                 if (results.length > 0) {
                     let user = results[0];
-                    resolve(new User(
-                        user.email,
-                        user.fullName,
-                        user.teleNumber,
-                        user.sessionID
-                    ));
+                    user.inDB = true;
+                    // populate PhoneNumbers
+                    dbConnection.makeQuery(
+                        'SELECT TelephoneNumber FROM PhoneNumbers WHERE Email=?',
+                        [ email ]
+                    ).then(([ results ]) => {
+                        user.telephoneNumbers = [];
+                        for (let index = 0; index < results.length; index++) {
+                            const element = results[index];
+                            user.telephoneNumbers.push(element.TelephoneNumber);
+                        }
+                        resolve(new User(user));
+                    }).catch((err) => reject(err));
+                } else {
+                    resolve(null);
                 }
-                resolve(null);
+            }).catch((err) => reject(err));
+        });
+    }
+
+    async save() {
+        return new Promise((resolve, reject) => {
+            dbConnection.makeQuery(
+                'DELETE FROM PhoneNumbers WHERE Email=?',
+                [ this.email ]
+            ).then(async () => {
+                for (let index = 0; index < this.telephoneNumbers.length; index++) {
+                    const telephoneNumber = this.telephoneNumbers[index];
+                    await dbConnection.makeQuery(
+                        'INSERT INTO PhoneNumbers Email=?, TelephoneNumber=?',
+                        [ this.email, telephoneNumber ]
+                    );
+                }
+                if (this.inDB) {
+                    dbConnection.makeQuery(
+                        'UPDATE TileUsers SET Email=?, Password=?, FullName=?, Role=? WHERE Email=?',
+                        [ this.email, this.password, this.fullName, this.role, this.email ]
+                    ).then(() => {
+                        resolve(this);
+                    }).catch((err) => reject(err));
+                } else {
+                    dbConnection.makeQuery(
+                        'INSERT INTO TileUsers Email=?, Password=?, FullName=?, Role=?',
+                        [ this.email, this.password, this.fullName, this.role ]
+                    ).then(() => {
+                        this.inDB = true;
+                        resolve(this);
+                    }).catch((err) => reject(err));
+                }
             }).catch((err) => reject(err));
         });
     }
 }
+
+module.exports = User;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const createTables = require('./db/create_tables').createTables;
+const User = require('./db/models/user');
 
 const app = express();
 
-app.listen(process.env.HTTPS_PORT, () => {
+app.listen(process.env.HTTPS_PORT, async () => {
     console.log('Starting server');
-    createTables();
+    await createTables();
+
+    let user = await User.findByEmail('yahyaarakil@gmail.com');
+    console.log(user);
 });

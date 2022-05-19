ff9be06ddb7c910b0a57917b45fe438ec0c0b257
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-05-19 19:50:55 +0300

    testbench

--- server/server.js
+++ server/server.js
@@ -12,52 +12,354 @@ app.listen(process.env.HTTPS_PORT, async () => {
     console.log('Starting server');
     await createTables();
 
+    //------------------------------------------------------------------
 
-    // let material = new Material({
-    //     Code: 1,
-    //     Company: "DYO",
-    //     Name: "BOYA",
-    //     Price: 123
-    // });
+    let redPaint = new Material({
+        Code: 1,
+        Company: "DYO",
+        Name: "RED PAINT",
+        Price: 102.50
+    });
 
-    // let material1 = new Material({
-    //     Code: 2,
-    //     Company: "GLAZEMASTER",
-    //     Name: "SIR a",
-    //     Price: 44
-    // });
+    try {
+        await redPaint.save();
+    } catch (error) {
+        console.log("it is in DB");
+    }
 
-    // let material2 = new Material({
-    //     Code: 3,
-    //     Company: "GLAZEMASTER",
-    //     Name: "SIR e",
-    //     Price: 542.5,
-    //     Alternative: material1
-    // });
+    let greenPaint = new Material({
+        Code: 2,
+        Company: "DYO",
+        Name: "GREEN PAINT",
+        Price: 82.50
+    });
 
-    // try {
-    //     await material.save();
-    // } catch (error) {
-    //     console.log("it is in DB");
-    // }
+    try {
+        await greenPaint.save();
+    } catch (error) {
+        console.log("it is in DB");
+    }
 
-    // try {
-    //     await material1.save();
-    // } catch (error) {
-    //     console.log("it is in DB");
-    // }
+    let bluePaint = new Material({
+        Code: 3,
+        Company: "FILLI BOYA",
+        Name: "BLUE PAINT",
+        Price: 222.50
+    });
 
-    // try {
-    //     await material2.save();
-    // } catch (error) {
-    //     console.log("it is in DB");
-    // }
+    try {
+        await bluePaint.save();
+    } catch (error) {
+        console.log("it is in DB");
+    }
 
-    // let materials = await Material.findAll();
-    // console.log(materials);
+    let glaze1 = new Material({
+        Code: 4,
+        Company: "GLAZEMASTER",
+        Name: "GLAZE a",
+        Price: 44.43
+    });
 
-    // let materialX = await Material.findByCode(3);
-    // console.log(materialX);
+    try {
+        await glaze1.save();
+    } catch (error) {
+        console.log("it is in DB");
+    }
+
+    let glaze2 = new Material({
+        Code: 5,
+        Company: "GLAZEMASTER",
+        Name: "GLAZE e",
+        Price: 542.5,
+        Alternative: glaze1
+    });
+
+    try {
+        await glaze2.save();
+    } catch (error) {
+        console.log("it is in DB");
+    }
+
+    let clay = new Material({
+        Code: 6,
+        Company: "CLAY",
+        Name: "CLAY a",
+        Price: 22.5,
+    });
+
+    try {
+        await clay.save();
+    } catch (error) {
+        console.log("it is in DB");
+    }
+
+    //------------------------------------------------------------------
+
+    let user = new User({
+        Email: 'oguzkaganaltas@gmail.com',
+        Password: 'lol123',
+        FullName: 'Oguz Kagan Altas',
+        Role: 0,
+        TelephoneNumbers: [
+            '1',
+            '2'
+        ]
+    });
+
+    try {
+        await user.save();
+    } catch (error) {
+        console.log("it is in DB");
+    }
+
+    let user1 = new User({
+        Email: 'yahyaarakil@gmail.com',
+        Password: 'lol123lol',
+        FullName: 'Yahya Arakil',
+        Role: 1,
+        TelephoneNumbers: [
+            '3'
+        ]
+    });
+
+    try {
+        await user1.save();
+    } catch (error) {
+        console.log("it is in DB");
+    }
+
+    let user2 = new User({
+        Email: 'yyeliz@gmail.com',
+        Password: 'lol123321lol',
+        FullName: 'Yeliz Yeşilada',
+        Role: 2,
+        TelephoneNumbers: [
+            '4'
+        ]
+    });
+
+    try {
+        await user2.save();
+    } catch (error) {
+        console.log("it is in DB");
+    }
+
+    //------------------------------------------------------------------
+
+    let dept = new Deparment({
+        Name: 'RD',
+        Manager: user
+    });
+
+    try {
+        await dept.save();
+    } catch (error) {
+        console.log("it is in DB");
+    }
+    
+    let dept1 = new Deparment({
+        Name: 'Planning',
+        Manager: user
+    });
+
+    try {
+        await dept1.save();
+    } catch (error) {
+        console.log("it is in DB");
+    }
+
+    let dept2 = new Deparment({
+        Name: 'Production',
+        Manager: user
+    });
+
+    try {
+        await dept2.save();
+    } catch (error) {
+        console.log("it is in DB");
+    }
+
+    //------------------------------------------------------------------
+
+    let recipe = new Recipe({
+        Name: 'recipe',
+        Size: '3x3',
+        CreatedBy: user,
+        MoldShape: 'moldshape1',
+        BakerName: 'baker1',
+        InitTemp: 25.5,
+        Humidity: 20,
+        DryingDuration: 50,
+        DryingTemp: 90,
+        BakingDuration: 120,
+        BakingTemp: 50,
+        Alternative: false
+    });
+    try {
+        await recipe.save();
+    } catch (error) {
+        console.log("it is in DB");
+    }
+
+    let recipe1 = new Recipe({
+        Name: 'recipe1',
+        Size: '50x60',
+        CreatedBy: user,
+        MoldShape: 'moldshape3434',
+        BakerName: 'baker21',
+        InitTemp: 235.5,
+        Humidity: 203,
+        DryingDuration: 450,
+        DryingTemp: 160,
+        BakingDuration: 920,
+        BakingTemp: 660,
+        Alternative: false
+    });
+    try {
+        await recipe1.save();
+    } catch (error) {
+        console.log("it is in DB");
+    }
+
+    let recipe2 = new Recipe({
+        Name: 'recipe3',
+        Size: '80x60',
+        CreatedBy: user,
+        MoldShape: 'moldshape34',
+        BakerName: 'baker3',
+        InitTemp: 215.5,
+        Humidity: 210,
+        DryingDuration: 150,
+        DryingTemp: 910,
+        BakingDuration: 1210,
+        BakingTemp: 150,
+        Alternative: false
+    });
+    try {
+        await recipe2.save();
+    } catch (error) {
+        console.log("it is in DB");
+    }
+
+    //------------------------------------------------------------------
+
+    let comment = new Comment({
+        Comment: "very good",
+        Manager: user,
+        Recipe: recipe
+    })
+    try {
+        await comment.save();
+    } catch (error) {
+        console.log("it is in DB");
+    }
+
+    let comment1 = new Comment({
+        Comment: "very bad",
+        Manager: user1,
+        Recipe: recipe
+    })
+    try {
+        await comment1.save();
+    } catch (error) {
+        console.log("it is in DB");
+    }
+
+    let comment2 = new Comment({
+        Comment: "excellent work good",
+        Manager: user2,
+        Recipe: recipe1
+    })
+    try {
+        await comment2.save();
+    } catch (error) {
+        console.log("it is in DB");
+    }
+
+    //------------------------------------------------------------------
+
+    recipe.addMaterial(clay,{
+        amount:104,
+        applicationType:"fast",
+        waterContent: 123,
+        density: 1.23,
+        viscosity: 3.12
+    })
+
+    recipe.addMaterial(glaze1,{
+        amount:34,
+        applicationType:"slow",
+        waterContent: 75,
+        density: 1.23,
+        viscosity: 3.12
+    })
+
+    recipe.addMaterial(glaze2,{
+        amount:104,
+        applicationType:"normal",
+        waterContent: 66,
+        density: 1.2213,
+        viscosity: 6.12
+    })
+
+    recipe.save();
+
+
+
+    recipe1.addMaterial(clay,{
+        amount:104,
+        applicationType:"fast",
+        waterContent: 123,
+        density: 1.23,
+        viscosity: 3.12
+    })
+
+    recipe1.addMaterial(glaze1,{
+        amount:34,
+        applicationType:"slow",
+        waterContent: 75,
+        density: 1.23,
+        viscosity: 3.12
+    })
+
+    recipe1.save();
+
+    recipe2.addMaterial(clay,{
+        amount:34,
+        applicationType:"slow",
+        waterContent: 75,
+        density: 1.23,
+        viscosity: 3.12
+    })
+
+    recipe2.addMaterial(glaze2,{
+        amount:104,
+        applicationType:"normal",
+        waterContent: 66,
+        density: 1.2213,
+        viscosity: 6.12
+    })
+
+    recipe2.save();
+
+    //------------------------------------------------------------------
+
+    recipe.addPaint(redPaint, {
+        grammage: 0.512
+    })
+    recipe1.addPaint(greenPaint, {
+        grammage: 0.6666
+    })
+    recipe2.addPaint(bluePaint, {
+        grammage: 0.912
+    })
+
+    //------------------------------------------------------------------
+
+    //------------------------------------------------------------------
+    console.log(await Material.getMaterials(0,1));
+    console.log(await Material.findByCode(3));
+    console.log(await Material.getMaterialsCount());
 
     // try {
     //     let a = await Recipe.findRecipesContainsMaterialCode(1);
@@ -66,75 +368,5 @@ app.listen(process.env.HTTPS_PORT, async () => {
     //     console.log("it is in DB");
     // }
 
-    // let user = await User.findByEmail('oguzkaganaltas@gmail.com');
-    // console.log(user);
-    
-    // let user = await User.findByEmail('oguzkaganaltas@gmail.com');
-    // let user = await User.findBySessionID('123');
-    // let user = new User({
-    //     Email: 'oguzkaganaltas@gmail.com',
-    //     Password: 'lol123',
-    //     FullName: 'Oguz Kagan Altas',
-    //     Role: 0,
-    //     TelephoneNumbers: [
-    //         '1',
-    //         '2'
-    //     ]
-    // });
-    // console.log(user);
-    // await user.save();
-    // console.log(user);
 
-    // let dept = new Deparment({
-    //     Name: 'RD',
-    //     Manager: user
-    // });
-    // console.log(dept);
-    // await dept.save();
-    // console.log(dept);
-
-    // let recipe = new Recipe({
-    //     Name: 'recipe4',
-    //     Size: '3x3',
-    //     CreatedBy: user,
-    //     MoldShape: 'moldshape1',
-    //     BakerName: 'baker1',
-    //     InitTemp: 25.5,
-    //     Humidity: 20,
-    //     DryingDuration: 50,
-    //     DryingTemp: 90,
-    //     BakingDuration: 120,
-    //     BakingTemp: 50
-    // });
-    // await recipe.save();
-
-    // let user = await User.findByEmail("oguzkaganaltas@gmail.com");
-
-    // let recipe = await Recipe.findByID(1);
-
-    // console.log(user);
-    // console.log(recipe);
-
-    // let comment = new Comment({
-    //     Comment: "very good",
-    //     Manager: user,
-    //     Recipe: recipe
-    // })
-    // await comment.save();
-
-
-    // // let recipe1 = await Recipe.findByID(1);
-
-    // // let recipe2 = await Recipe.findByID(2);
-    // // recipe2.previousVersion = recipe1;
-    // // recipe.unapproval.isRejected = true;
-    // recipe.approved = true;
-    // await recipe.save();
-    // console.log(recipe);
-    await Recipe.getRecipesCount();
-    // let recipe = await Recipe.findByID(8);
-    // recipe.isRejected = true;
-    // recipe.rejectionDate = Date.now();
-    // await recipe.save();
-    // console.log(recipe);
 });

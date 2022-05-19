27cf34abeba08ce3302ca2874309af57ac43b539
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-05-19 10:28:43 +0300

    sessionId length fixed

--- server/db/create_tables.js
+++ server/db/create_tables.js
@@ -15,7 +15,7 @@ tableCreators = [
    async () => {
         return await dbConnection.makeQuery(`CREATE TABLE IF NOT EXISTS SessionIDs (
             Email VARCHAR(50) NOT NULL,
-            SessionID VARCHAR(1000) NOT NULL,
+            SessionID VARCHAR(256) NOT NULL,
 
             PRIMARY KEY (Email, SessionID),
             FOREIGN KEY (Email) REFERENCES TileUsers(Email)

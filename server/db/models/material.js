dee285da7c392375c589ce2eaa32f1639fa10963
GreatSum <42463375+oguzkaganaltas@users.noreply.github.com>
2022-05-19 09:28:53 +0300

    Material model added

---
+++ server/db/models/material.js
@@ -0,0 +1,56 @@
+const dbConnection = require('../db_connection');
+
+class Material {
+    constructor({ Code, Company, Name, Price, Alternative, inDB }) {
+        this.code = Code;
+        this.company = Company;
+        this.name = Name;
+        this.price = Price;
+        this.alternative = Alternative ? Alternative : null;
+        this.inDB = inDB ? inDB : false;
+    }
+
+    static findBy(key, value) {
+        try {
+            let [results] = await dbConnection.makeQuery(`SELECT * FROM Materials WHERE ${key}=?;`, [value]);
+            if (results.length > 0) {
+                let material = results[0];
+                material.inDB = true;
+
+                if (material.alternative !== null) {
+                    material.alternative = await Material.findByCode(material.alternative);
+                }
+                return new Material(material);
+            }
+            else {
+                return null;
+            }
+
+        } catch (error) {
+            throw error
+        }
+    }
+
+    static async findByCode(code) {
+        return await this.findBy('Code', code);
+    }
+
+    async save() {
+        try {
+            if (this.inDB) {
+                await dbConnection.makeQuery('UPDATE Materials SET Code=?, Company=?, Name=?, Price=?, Alternative=?, WHERE Code=?;', [this.code, this.company, this.name, this.price, this.alternative, this.code]);
+            } else {
+                await dbConnection.makeQuery(
+                    'INSERT INTO Materials (Code, Company, Name, Price, Alternative) VALUES (?, ?, ?, ?, ?);',
+                    [this.code, this.company, this.name, this.price, this.alternative]
+                );
+                this.inDB = true;
+            }
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+
+}
